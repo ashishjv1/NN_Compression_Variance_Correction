7e8300da926585f7e0bcf76ae059522e02029196
Ashish Jha <ashish.jhav1@hotmail.com>
2023-06-01 20:58:15 +0300

    Plot Update

--- plot_dist.py
+++ plot_dist.py
@@ -3,16 +3,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_dist(weight_list, ylim=(0,0.75), xlim=(-0.4, 1.5)):
+def plot_dist(weight_list, ylim=(0,0.75), xlim=(-0.4, 1.5), fill=False, alpha=0.6):
 
         linestyle = ['-', '-.', '--', '.']
-        color = ['black', 'lightgreen', 'blue', 'red']
+        color = ['lightblue', 'lightgreen', 'blue', 'red']
         label = ['Original Layer', 'FL1', 'FL2', 'FL3', 'FL4']
-        alpha = [0.4, 0.2, 0.2]
 
-        plt.figure(figsize=(12,12), dpi=100)
+        plt.figure(figsize=(8,8), dpi=100)
         for i in range(len(weight_list)):
-            ax = sns.kdeplot(weight_list[i][0:1000].flatten().detach().numpy()) 
+            ax = sns.kdeplot(weight_list[i][0:1000].flatten().detach().numpy(), fill=fill, alpha=alpha,
+                              linewidth=1.1, linestyle=linestyle[i], edgecolor="black", color=color[i], label=label[i]) 
         plt.ylim(ylim)
         plt.xlim(xlim)
         plt.legend()
